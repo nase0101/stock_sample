da10319317588261779e912242802d8bd27d49f4
Jim <jim@ChendeMacBook-Pro.local>
2016-07-03 22:38:20 +0800

    Basic stock and price method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-  	
+  	@stock = Stock.show_stock
   end
 end
---
+++ app/models/stock.rb
@@ -0,0 +1,22 @@
+class Stock < ActiveRecord::Base
+	
+  def self.show_stock
+  	looked_up_stock      = StockQuote::Stock.quote('GOOG')
+  	new_stock            = Stock.new
+  	new_stock.ticker     = "#{looked_up_stock.symbol}"
+  	new_stock.name       = "#{looked_up_stock.name}"
+  	new_stock.last_price = new_stock.price
+
+  	return new_stock
+  end
+
+  def price
+  	closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (Closing)" if closing_price
+    
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (Opening)" if opening_price
+
+    return 'Unavailable'
+  end
+end
