d1c3227ffd292dc813f28f9791b8e5141753b6e1
Jim <jim@ChendeMacBook-Pro.local>
2016-07-06 22:17:44 +0800

    Now can search empty string

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,20 @@
 class WelcomeController < ApplicationController
   def index
-  	@stock = Stock.show_stock
+  	@stock = Stock.search_stock(params[:stock])
+  	#@stock.name = "no"
+    #@stock.ticker = "nono"
+    #@stock.last_price = -1
+    if @stock
+      #render json: @stock
+      #render partial: 'lookup'
+      
+    else
+      #render  status: :not_found, nothing: true
+      
+    end
+  end
+
+  def search
+
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,32 @@
 class Stock < ActiveRecord::Base
-	
+
   def self.show_stock
   	looked_up_stock      = StockQuote::Stock.quote('GOOG')
   	new_stock            = Stock.new
   	new_stock.ticker     = "#{looked_up_stock.symbol}"
   	new_stock.name       = "#{looked_up_stock.name}"
   	new_stock.last_price = new_stock.price
+
+  	return new_stock
+  end
+
+  def self.search_stock(ticker)
+  	if !ticker.to_s==''
+  		looked_up_stock      = StockQuote::Stock.quote(ticker)
+  		return nil unless looked_up_stock.name
+
+  		new_stock            = Stock.new
+  		new_stock.ticker     = "#{looked_up_stock.symbol}"
+  		new_stock.name       = "#{looked_up_stock.name}"
+  		new_stock.last_price = new_stock.price
+    else
+    	new_stock            = Stock.new
+  		new_stock.ticker     = "nono"
+  		new_stock.name       = "no"
+  		new_stock.last_price = -1
+
+    end
+
 
   	return new_stock
   end
